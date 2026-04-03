b8803b8ab21c53d47ddae923aedde38deba17629
NBwrt <wangdf852@gmail.com>
2026-04-03 17:33:20 +0800

    diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -2,17 +2,18 @@
 #
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
 #
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# 【关键修复】删除所有 iStore 相关源（你要求删除iStore）
+sed -i '/istore/d' feeds.conf.default
 
-# Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# 【关键修复】只保留必需源，删除冗余nas源（避免找不到目录）
+# 注释掉nas源，因为你已经删除iStore，不需要nas依赖
+# echo 'src-git nas https://github.com/linkease/nas-packages.git;main' >> feeds.conf.default
+# echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.default
+
+# 启用 helloworld 源（OpenClash 必需）
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# 【可选】如果需要OpenList，添加对应源（按需开启）
+echo 'src-git openlist https://github.com/immortalwrt/openwrt-openlist.git;main' >> feeds.conf.default
